9cff032417147b25913b7bef925ed65f05aeb48d
Guillermo Garron <guillermo@mac>
2014-12-27 10:24:33 -0400

    first version of index.php

---
+++ index.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="<?php bloginfo( 'charset' ); ?>">
+  <title><?php wp_title( '|', true, 'right' ); ?></title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+	<!--link rel="stylesheet/less" href="less/bootstrap.less" type="text/css" /-->
+	<!--link rel="stylesheet/less" href="less/responsive.less" type="text/css" /-->
+	<!--script src="js/less-1.3.3.min.js"></script-->
+	<!--append ‘#!watch’ to the browser URL, then refresh the page. -->
+	
+    <link rel="stylesheet" href="<?php echo esc_url( get_stylesheet_uri() ); ?>" type="text/css" />
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<link href="css/style.css" rel="stylesheet">
+
+  <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+  <!--[if lt IE 9]>
+    <script src="js/html5shiv.js"></script>
+  <![endif]-->
+
+  <!-- Fav and touch icons -->
+  <link rel="apple-touch-icon-precomposed" sizes="144x144" href="img/apple-touch-icon-144-precomposed.png">
+  <link rel="apple-touch-icon-precomposed" sizes="114x114" href="img/apple-touch-icon-114-precomposed.png">
+  <link rel="apple-touch-icon-precomposed" sizes="72x72" href="img/apple-touch-icon-72-precomposed.png">
+  <link rel="apple-touch-icon-precomposed" href="img/apple-touch-icon-57-precomposed.png">
+  <link rel="shortcut icon" href="img/favicon.png">
+  
+	<script type="text/javascript" src="js/jquery.min.js"></script>
+	<script type="text/javascript" src="js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="js/scripts.js"></script>
+    <?php wp_head(); ?>
+</head>
+
+<body>
+<div class="container">
+	<div class="row clearfix">
+		<div class="col-md-12 column">
+			<h1 class="text-center">
+                <?php bloginfo( 'name' ); ?>
+            </h1>
+            <h2 class="text-center">
+                <?php bloginfo( 'description' ); ?>
+			</h2>
+		</div>
+	</div>
+	<div class="row clearfix">
+		<div class="col-md-12 column">
+            <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+ 
+            <h3><?php the_title(); ?></h3>
+ 
+            <?php the_content(); ?>
+            <?php wp_link_pages(); ?>
+            <?php edit_post_link(); ?>
+ 
+            <?php endwhile; ?>
+ 
+            <?php
+                if ( get_next_posts_link() ) {
+                    next_posts_link();
+                }
+            ?>
+            <?php
+                if ( get_previous_posts_link() ) {
+                    previous_posts_link();
+                }
+            ?>
+ 
+            <?php else: ?>
+ 
+                <p>No posts found. :(</p>
+ 
+            <?php endif; ?>
+		</div>
+	</div>
+	<div class="row clearfix">
+		<div class="col-md-12 column">
+            <?php wp_footer(); ?>
+		</div>
+	</div>
+</div>
+</body>
+</html>
