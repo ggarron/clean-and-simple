43f8060903849f969502bfbc276631df4228e2d4
Guillermo Garron <guillermo@mac>
2014-12-28 09:44:35 -0400

    making index.php a copy of home.php, now that we have single.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 	<div class="row clearfix">
 		<div class="col-md-12 column">
 			<h1 class="text-center">
-                <a href="<?php echo site_url('/'); ?>"><?php bloginfo( 'name' ); ?></a>
+                <?php bloginfo( 'name' ); ?>
             </h1>
             <h2 class="text-center">
                 <?php bloginfo( 'description' ); ?>
@@ -16,7 +16,7 @@
         <div class="col-md-3 column"></div>
 		<div class="col-md-6 column">
             <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-            <h3><center><?php the_title(); ?></center></h3>
+            <h3><center><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></center></h3>
             <small><p>Categories: <?php the_category( ', ' ); ?></p></small>
             <small><p><?php the_tags(); ?></p></small>
             <hr>
