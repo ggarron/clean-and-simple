645d40ab47eb0f1b1263e994925bc0661d9823f4
Guillermo Garron <guillermo@mac>
2014-12-28 00:43:38 -0400

    listing posts in an unordered list

--- home.php
+++ home.php
@@ -15,11 +15,9 @@
 	<div class="row clearfix">
 		<div class="col-md-12 column">
             <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-            <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
- 
-                <?php the_post_thumbnail(); ?>
-                <?php the_excerpt(); ?>
-
+            <ul>
+                <li><h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2></li>
+            </ul>
             <?php endwhile; else: ?>
             <?php _e('Sorry, no posts matched your criteria.'); ?>
             <?php endif; ?>
