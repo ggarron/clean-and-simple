5d0af43d46ea63a2dc2ddb1ebf4dc2e65003409d
Guillermo Garron <guillermo@mac>
2014-12-28 08:38:26 -0400

    adding link to home for title of blog

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
   <meta charset="<?php bloginfo( 'charset' ); ?>">
-  <title><?php wp_title( '|', true, 'right' ); ?></title>
+    <title><a href="<?php echo site_url('/'); ?>"><?php wp_title( '|', true, 'right' ); ?></a></title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="description" content="">
   <meta name="author" content="">
