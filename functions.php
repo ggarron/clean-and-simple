ebca7856118b93daa609862787bd4aa49e766fb6
Guillermo Garron <guillermo@mac>
2014-12-28 10:27:22 -0400

    fixing function to manage home title

--- functions.php
+++ functions.php
@@ -8,7 +8,7 @@ add_filter( 'wp_title', 'baw_hack_wp_title_for_home' );
 function baw_hack_wp_title_for_home( $title )
 {
   if( empty( $title ) && ( is_home() || is_front_page() ) ) {
-    return __( 'Home', 'theme_domain' ) . ' | ' . get_bloginfo( 'description' );
+    return __( 'Home', 'theme_domain' ) . ' | ' . get_bloginfo( 'name' );
   }
   return $title;
 }
