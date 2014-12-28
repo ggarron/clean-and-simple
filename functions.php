50e51aa8a4a033e0480ae50e069e8eee843ced6e
Guillermo Garron <guillermo@mac>
2014-12-28 10:23:45 -0400

    adding function to manage home title

--- functions.php
+++ functions.php
@@ -3,4 +3,14 @@ function child_scripts_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css', array() );
 }
 add_action( 'wp_enqueue_scripts', 'child_scripts_styles' );
+
+add_filter( 'wp_title', 'baw_hack_wp_title_for_home' );
+function baw_hack_wp_title_for_home( $title )
+{
+  if( empty( $title ) && ( is_home() || is_front_page() ) ) {
+    return __( 'Home', 'theme_domain' ) . ' | ' . get_bloginfo( 'description' );
+  }
+  return $title;
+}
+
 ?>
