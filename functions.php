53198659f66a55c92d095da85ab4cde9a4653bf5
Guillermo Garron <guillermo@mac>
2014-12-28 15:53:42 -0400

    playing with background colors

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
 function child_scripts_styles() {
-    wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css', array(), '0.2' );
+    wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css', array(), '0.3' );
 }
 add_action( 'wp_enqueue_scripts', 'child_scripts_styles' );
 
