3ef937b5d6be499f0af58193ab1d1a2fe1874dca
Guillermo Garron <guillermo@mac>
2014-12-28 00:33:32 -0400

    adding link to post

--- home.php
+++ home.php
@@ -15,7 +15,7 @@
 	<div class="row clearfix">
 		<div class="col-md-12 column">
             <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-                <h2><?php the_title(); ?></h2>
+            <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
  
                 <?php the_post_thumbnail(); ?>
                 <?php the_excerpt(); ?>
