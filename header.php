02761cb500cab1886fd03d69e1be824710ef38b4
Guillermo Garron <guillermo@mac>
2015-01-03 23:16:52 -0400

    unlinking a non existent file

--- header.php
+++ header.php
@@ -29,6 +29,5 @@
   
 	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 	<script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/bootstrap.min.js"></script>
-	<script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/scripts.js"></script>
     <?php wp_head(); ?>
 </head>
